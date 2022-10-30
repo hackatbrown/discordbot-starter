8437ce6020baaa929836dd985382caba6b999dd0
Ayma-n <aymanbt14@gmail.com>
2022-10-30 18:01:40 -0400

    added IntentOptions file and config folder, with explanations

---
+++ src/config/IntentOptions.ts
@@ -0,0 +1,16 @@
+import { GatewayIntentBits } from "discord.js";
+
+/**
+ * This array should contain all events that you want your bot to be able to receive.
+ * Basically, you are specifying what information your Discord bot intends to access in order to function properly.
+ * 
+ * A more detailed explanation of intents can be found on the Discord.js documentation:
+ * https://discordjs.guide/popular-topics/intents.html#privileged-intents
+ * 
+ * You can also find a list of all intents in the Discord developer docs at the following address:
+ * https://discord.com/developers/docs/topics/gateway#list-of-intents
+ */
+
+export const IntentOptions = [
+    GatewayIntentBits.Guilds,
+];
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// This line imports the Client class, which is going to represent your Discord bot's session.
+import { Client } from "discord.js";
+import { IntentOptions } from "./config/IntentOptions";
+
+(async () => {
+    const BOT = new Client({intents: IntentOptions});
+    await BOT.login(process.env.BOT_TOKEN);
+})
+
+
+
