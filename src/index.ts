f0d3255112ec63d2155dee017286fff1e122fb72
Ayma-n <aymanbt14@gmail.com>
2022-11-06 17:15:32 -0500

    added comments and listening to ready event

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import { Client } from "discord.js";
 import { IntentOptions } from "./config/IntentOptions";
 
 (async () => {
+    // We create the bot object, and login to Discord's servers with the bot token provided
+    // in .env.
     const BOT = new Client({intents: IntentOptions});
     await BOT.login(process.env.BOT_TOKEN);
+
+    // The .on() syntax is used when we want the bot to listen to a certain event.
+    // Here, we listen to the ready event, which happens when the Discord bot Client is ready.
+    BOT.on("ready", () => console.log("Bot is connected to Discord!"));
 })
