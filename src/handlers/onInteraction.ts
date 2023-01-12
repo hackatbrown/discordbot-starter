6180f146caf5687ead9a4d41c92614824affefa1
Ayman Benjelloun Touimi <aymanbt14@gmail.com>
2023-01-12 17:03:55 +0100

    added extra comments in onInteraction file

--- src/handlers/onInteraction.ts
+++ src/handlers/onInteraction.ts
@@ -2,12 +2,25 @@ import { CommandInteraction, Interaction } from "discord.js";
 import fs from "fs";
 import { ICommand } from "../interfaces/ICommand";
 
+/**
+ * This function corresponds to the main routine that gets run when an interaction
+ * is handled. One of the most important types of interaction are commands.
+ * In this file, we have set up a function for you, "handleCommand", which takes
+ * care of identifying which command a user sent, and running the appropriate code.
+ */
 export const onInteraction = async (interaction : Interaction) => {
     if (interaction.isCommand()) {
         handleCommand(interaction);
     }
 }
 
+/**
+ * handleCommand looks for all command files in the commands directory,
+ * and checks whether the CommandInteraction given to it as a parameter
+ * is in the list of defined commands for the bot. 
+ * If it exists, it runs the "execute" command, defined in the file
+ * corresponding to the user's inputted command.
+ */
 const handleCommand = (interaction : CommandInteraction) => {
     const commandFiles : string[] = fs.readdirSync('./commands/').filter(file => file.endsWith(".js"))
     
