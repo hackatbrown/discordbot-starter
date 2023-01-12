1e8e10e8754527ad07abf38a9122c66a35d5a860
Ayman Benjelloun Touimi <aymanbt14@gmail.com>
2023-01-12 17:12:24 +0100

    added ping command

---
+++ src/commands/ping.ts
@@ -0,0 +1,16 @@
+import { CommandInteraction, Interaction, SlashCommandBuilder } from "discord.js";
+import { ICommand } from "../interfaces/ICommand";
+
+const pingCommand : ICommand = {
+    data: new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("A basic ping command, meant to check that the bot works properly!"),
+    
+    execute: async (interaction) => {
+        interaction.reply("pong!")
+    }
+}
+
+module.exports = {
+    command: pingCommand,
+}
